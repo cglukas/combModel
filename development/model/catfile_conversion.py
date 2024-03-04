9fd04e8d4b36d486937960411a6d8b0f815da58e
Lukas Wieg <lukaswieg@googlemail.com>
2024-03-04 14:22:26 +0100

    Implement NukeModel class for nuke conversion.

--- development/model/catfile_conversion.py
+++ development/model/catfile_conversion.py
@@ -1,8 +1,37 @@
+"""Conversion process to generate a nuke compliant cat file."""
 from pathlib import Path
 
 import torch
+from torch import nn
 
 from development.model.comb_model import CombModel
+
+
+class NukeModel(nn.Module):
+    """Nuke implementation of the comb model.
+
+    This class is used for converting the pytorch model to a 'catfile' that
+    can be executed inside nuke.
+    """
+
+    def __init__(self, model: CombModel):
+        super().__init__()
+        self.model = model
+        self.level = 1
+        self.person = 1
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        """Process an input image with the loaded model.
+
+        Args:
+            image: tensor of the input image [1, channels, height, width]
+
+        Returns:
+            processed image as tensor [1, channels, height, width]
+        """
+        return self.model.progressive_forward(
+            person=self.person, batch=image, level=self.level, last_level_influence=1
+        )
 
 
 def convert_model(model_checkpoint: Path, export_path: Path) -> None:
--- tests/test_catfile_conversion.py
+++ tests/test_catfile_conversion.py
@@ -1,3 +1,4 @@
+"""Tests for the catfile conversion process."""
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from unittest.mock import MagicMock, patch
@@ -5,8 +6,18 @@ from unittest.mock import MagicMock, patch
 import pytest
 import torch
 
-from development.model.catfile_conversion import convert_model
+from development.model.catfile_conversion import NukeModel, convert_model
 from development.model.comb_model import CombModel
+
+
+def test_nuke_model_forward() -> None:
+    """Test that the forward method returns a tensor of the same size."""
+    model = CombModel()
+    test_input = torch.zeros(1, 3, 128, 128)
+
+    processed = NukeModel(model).forward(test_input)
+
+    assert processed.shape == test_input.shape
 
 
 @pytest.mark.parametrize("persons", [1, 2, 4])

7d19b085a36a863f425d77334c335a0a3784f7b0
Lukas Wieg <lukaswieg@googlemail.com>
2024-03-08 14:07:49 +0100

    Add module doc and remove grouping by class.

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,3 +1,4 @@
+"""Tests for the encoder neuronal network."""
 import pytest
 import torch
 
@@ -5,36 +6,35 @@ from development.model.encoder import Encoder
 from test_comb_model import _levels
 
 
-class TestEncoder:
-    @pytest.mark.parametrize(("level", "size"), _levels)
-    def test_encoder(self, level: int, size: int):
-        """Test that all encoder levels are encoded to the same latent tensor."""
-        rand = torch.ones((1, 3, size, size))
-        enc = Encoder()
+@pytest.mark.parametrize(("level", "size"), _levels)
+def test_encoder(level: int, size: int):
+    """Test that all encoder levels are encoded to the same latent tensor."""
+    rand = torch.ones((1, 3, size, size))
+    enc = Encoder()
 
-        latent = enc.forward(rand, level=level)
+    latent = enc.forward(rand, level=level)
 
-        assert (1, 512, 1, 1) == latent.shape
+    assert (1, 512, 1, 1) == latent.shape
 
-    @pytest.mark.parametrize(("level", "size"), _levels)
-    @pytest.mark.parametrize("last_lvl_influence", [0.0, 0.5, 1.0])
-    def test_encoder_progressive_forward(
-        self, level: int, size: int, last_lvl_influence: float
-    ):
-        """Test that the progressive forward encodes the input to the latent vector."""
-        rand = torch.ones((1, 3, size, size))
-        enc = Encoder()
 
-        latent = enc.progressive_forward(
-            rand, level=level, last_lvl_influence=last_lvl_influence
-        )
+@pytest.mark.parametrize(("level", "size"), _levels)
+@pytest.mark.parametrize("last_lvl_influence", [0.0, 0.5, 1.0])
+def test_encoder_progressive_forward(level: int, size: int, last_lvl_influence: float):
+    """Test that the progressive forward encodes the input to the latent vector."""
+    rand = torch.ones((1, 3, size, size))
+    enc = Encoder()
 
-        assert (1, 512, 1, 1) == latent.shape
+    latent = enc.progressive_forward(
+        rand, level=level, last_lvl_influence=last_lvl_influence
+    )
 
-    def test_encoder_torchscript(self):
-        """Test that the encoder can be scripted."""
-        enc = Encoder()
+    assert (1, 512, 1, 1) == latent.shape
 
-        scripted = torch.jit.script(enc)
 
-        assert isinstance(scripted, torch.jit.ScriptModule)
+def test_encoder_torchscript():
+    """Test that the encoder can be scripted."""
+    enc = Encoder()
+
+    scripted = torch.jit.script(enc)
+
+    assert isinstance(scripted, torch.jit.ScriptModule)

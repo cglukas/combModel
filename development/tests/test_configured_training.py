30b39a523a7d3c195ef20d3f82fd1fe32f26f844
Lukas Wieg <lukaswieg@googlemail.com>
2024-03-15 13:44:10 +0100

    Add implementation to initialize a new model.

--- development/tests/test_configured_training.py
+++ development/tests/test_configured_training.py
@@ -1,11 +1,19 @@
 """Tests for the training configuration."""
-from unittest.mock import ANY, MagicMock, call, patch
+from pathlib import Path
+from unittest.mock import MagicMock, patch
 
+import adabelief_pytorch.AdaBelief
 import pytest
 import torch
+from adabelief_pytorch import AdaBelief
 
 from development.data_io.dataset_manager import DatasetManager
-from development.trainer.configured_training import TrainingConfig, _load_datasets
+from development.model.comb_model import CombModel
+from development.trainer.configured_training import (
+    TrainingConfig,
+    _init_model_and_optimizer,
+    _load_datasets,
+)
 
 
 @pytest.mark.parametrize("datasets", [["test", "test2"], ["test"]])
@@ -29,3 +37,129 @@ def test_load_datasets_empty() -> None:
 
     with pytest.raises(ValueError, match="No datasets provided."):
         _load_datasets(conf)
+
+
+class TestInitModelAndOptimizer:
+    """Test cases for the init model and optimizer function."""
+
+    @pytest.fixture()
+    def sdg_instance(self) -> MagicMock:
+        """Get a mock instance for the sdg optimizer."""
+        return MagicMock(autospec=torch.optim.SGD)
+
+    @pytest.fixture()
+    def sgd_mock(self, sdg_instance) -> MagicMock:
+        """Mock the SGD optimizer"""
+        with patch("torch.optim.SGD") as sdg:
+            sdg.return_value = sdg_instance
+            yield sdg
+
+    @pytest.fixture()
+    def model_instance(self) -> MagicMock:
+        """Get a mock instance of the comb model."""
+        return MagicMock(autospec=CombModel)
+
+    @pytest.fixture()
+    def model_mock(self, model_instance) -> MagicMock:
+        """Mock the CombModel."""
+        with patch("development.trainer.configured_training.CombModel") as model_mock:
+            model_mock.return_value = model_instance
+            yield model_mock
+
+    @pytest.fixture()
+    def pretrain_init(self, model_instance) -> MagicMock:
+        """Mock the 'initialize from pretraining' function."""
+        with patch(
+            "development.trainer.configured_training.initialize_comb_model_from_pretraining"
+        ) as init_mock:
+            init_mock.return_value = model_instance
+            yield init_mock
+
+    @pytest.fixture()
+    def ada_belief_instance(self) -> MagicMock:
+        """Get a mock instance for the AdaBelief optimizer."""
+        return MagicMock(autospec=AdaBelief)
+
+    @pytest.fixture()
+    def ada_belief_mock(self, ada_belief_instance) -> MagicMock:
+        """Mock the AdaBelief optimizer."""
+        with patch(
+            "development.trainer.configured_training.AdaBelief"
+        ) as ada_belief_mock:
+            ada_belief_mock.return_value = ada_belief_instance
+            yield ada_belief_mock
+
+    def test_init_model_and_optimizer(
+        self,
+        model_mock: MagicMock,
+        model_instance: MagicMock,
+        sdg_instance: MagicMock,
+        pretrain_init: MagicMock,
+        sgd_mock: MagicMock,
+    ):
+        """Test that the model and optimizer can be initialized."""
+        conf = TrainingConfig(
+            datasets=["1", "2"],
+            learning_rate=2e-4,
+            pretraining_checkpoint="test_checkpoint",
+        )
+
+        model, optimizer = _init_model_and_optimizer(conf)
+
+        assert model is model_instance
+        assert optimizer is sdg_instance
+        sgd_mock.assert_called_with(model_instance.parameters(), lr=2e-4, momentum=0.9)
+        pretrain_init.assert_called_with(Path("test_checkpoint"), num_persons=2)
+
+    def test_without_checkpoint(
+        self,
+        model_instance: MagicMock,
+        model_mock: MagicMock,
+        sdg_instance: MagicMock,
+        pretrain_init: MagicMock,
+        sgd_mock: MagicMock,
+    ):
+        """Test that the model and optimizer can be initialized."""
+        conf = TrainingConfig(
+            datasets=["1", "2"],
+            learning_rate=2e-4,
+        )
+
+        model, optimizer = _init_model_and_optimizer(conf)
+
+        assert model is model_instance
+        assert optimizer is sdg_instance
+        model_mock.assert_called_with(persons=2)
+        sgd_mock.assert_called_with(model_instance.parameters(), lr=2e-4, momentum=0.9)
+        pretrain_init.assert_not_called()
+
+    def test_with_ada_belief(
+        self,
+        model_mock: MagicMock,
+        sgd_mock: MagicMock,
+        ada_belief_instance: MagicMock,
+        ada_belief_mock: MagicMock,
+    ):
+        """Test that the AdaBelief optimizer can be initialized."""
+        conf = TrainingConfig(
+            datasets=["1", "2"], learning_rate=2e-5, optimizer="AdaBelief"
+        )
+
+        _, optimizer = _init_model_and_optimizer(conf)
+
+        assert optimizer is ada_belief_instance
+        sgd_mock.assert_not_called()
+        ada_belief_mock.assert_called_once()
+        assert ada_belief_mock.call_args.kwargs["lr"] == 2e-5
+
+    def test_wrong_optimizer(
+        self, model_mock: MagicMock, sgd_mock: MagicMock, ada_belief_mock: MagicMock
+    ):
+        """Test that a value error is raised if the wrong optimizer class is used."""
+        wrong_conf = TrainingConfig(datasets=["1", "2"], optimizer="WrongOptimizer")
+
+        with pytest.raises(
+            ValueError,
+            match="Optimizer wrong: 'WrongOptimizer'. Possible optimizer: 'SGD', 'AdaBelief'",
+        ):
+            _init_model_and_optimizer(wrong_conf)
--- development/trainer/configured_training.py
+++ development/trainer/configured_training.py
@@ -1,30 +1,77 @@
 """File for training based on configuration files."""
 from dataclasses import dataclass, field
+from pathlib import Path
 
 import torch
+from adabelief_pytorch import AdaBelief
 from torch.optim import Optimizer
 
 from development.data_io.dataloader2 import PersonDataset
 from development.data_io.dataset_manager import DatasetManager
 from development.model.comb_model import CombModel
+from development.model.utils import initialize_comb_model_from_pretraining
 from development.trainer.level_manager import AbstractLevelManager
 from development.trainer.training_logger import TrainLogger
 
 
 @dataclass
 class TrainingConfig:
+    optimizer: str = "SGD"
+    """Optimizer class to use. Possible values: 'SGD', 'AdaBelief'. Will not be used when training is resumed."""
+    learning_rate: float = 10e-4
+    """The learning rate for the optimizer."""
+    trainings_folder: str = ""
+    """Folder where separate trainings are saved."""
+    pretraining_checkpoint: str = ""
+    """Checkpoint of the pretraining. This can't be used in combination with the resume checkpoint."""
+    resume_checkpoint: str = ""
+    """The model checkpoint used for resuming. This can't be used in combination with the pretraining_checkpoint"""
     device: str = "cpu"
     """The device where the training should run on."""
     datasets: list[str] = field(default_factory=list)
     """The list of datasets that should be considered for training."""
 
 
-def _load_model(config: TrainingConfig) -> CombModel:
+def _resume_training(config: TrainingConfig) -> tuple[CombModel, Optimizer]:
     pass
 
 
-def _load_optimizer(config: TrainingConfig) -> Optimizer:
-    pass
+def _init_model_and_optimizer(config: TrainingConfig) -> tuple[CombModel, Optimizer]:
+    """Initialize the model and optimizer based on the config.
+
+    If a pretraining is specified, this copies the pretraining to all decoders of the model.
+    """
+    persons = len(config.datasets)
+
+    if config.pretraining_checkpoint:
+        checkpoint = Path(config.pretraining_checkpoint)
+        model = initialize_comb_model_from_pretraining(checkpoint, num_persons=persons)
+    else:
+        model = CombModel(persons=persons)
+
+    if config.optimizer == "SGD":
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=config.learning_rate, momentum=0.9
+        )
+    elif config.optimizer == "AdaBelief":
+        # GAN(small) settings for the AdaBelief optimizer:
+        # more configurations can be found here:
+        # https://github.com/juntang-zhuang/Adabelief-Optimizer#hyper-parameters-in-pytorch
+        optimizer = AdaBelief(
+            model.parameters(),
+            lr=config.learning_rate,
+            betas=(0.5, 0.999),
+            eps=1e-12,
+            weight_decay=0,
+            weight_decouple=True,
+            rectify=False,
+            fixed_decay=False,
+        )
+    else:
+        msg = f"Optimizer wrong: '{config.optimizer}'. Possible optimizer: 'SGD', 'AdaBelief'."
+        raise ValueError(msg)
+
+    return model, optimizer
 
 
 def _load_logger(config: TrainingConfig) -> TrainLogger:
